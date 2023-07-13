9e4b9805b55d13c82f776aa8a9839707cd1c9dba
Darina Obukhova <darinaobukhova@darinas-mbp-2.kpn>
2023-07-14 00:07:41 +0200

    Edited scripts for clarity

--- scripts/05_deconvolution_preparation.R
+++ scripts/05_deconvolution_preparation.R
@@ -1,24 +1,50 @@
 #!/usr/bin/env Rscript
 
+#########################################################################################################################
+#' Author: Darina Obukhova
+#' Lab: Cellular Genomic Medicine, Maastricht University Medical Center (MUMC+)
+
+#' Script purpose: Create gene expression signature matrices from endometrium single-cell data subsets corresponding to the midsecretory
+#' phase of the menstrual cycle (GEO acc code: GSE111976), generated in the paper
+#' "Single cell RNA-seq analysis on human endometrium across the natural menstrual cycle" (PMID: 32929266). It is the
+#' first and most computation intensive part of the Dampened Weighted Least Squares (DWLS) method. Recommended to be
+#' carried out on the HPC.
+
+#' Input files: (1) C1 scRNA-seq dataset (Fluidigm) from GSE111976 
+#' (2) labels for C1 scRNA-seq dataset
+#' (3) 10X scRNA-seq dataset (10X Genomics) from GSE111976
+#' (4) labels for 10X scRNA-seq dataset 
+#' The paths to them are provided in the config file supplied from the command line
+
+#' Output files: Two signature matrices of C1 and 10X datasets, respectively (.RData)
+
+########################################################################################################################
+
 options(scipen=999)
+set.seed(746)
 
 args <- commandArgs(trailingOnly=TRUE)
 
-#Arguments
+# Arguments
+
 config <- args[1]
 source(as.character(config))
 
-#########################
-####Loading libraries####
-#########################
+# Loading packages
 
-library(dplyr); library(biomaRt); library(DESeq2); library(ggplot2); library(DWLS); library(ggpubr);
+library(dplyr)
+library(biomaRt)
+library(DESeq2)
+library(ggplot2)
+library(DWLS)
+library(ggpubr)
 library(Seurat)
 
 dir.create(file.path(outputdir, "results", Sys.Date()), showWarnings = F)
 setwd(file.path(outputdir, "results", Sys.Date()))
 
-#Single cell data
+# Single cell data
+
 cat("Loading single cell dataset...")
 C1 <- read.csv(C1_data, header = T)
 C1_labels <- read.csv(C1_labels, header = T)
@@ -27,28 +53,34 @@ tenx <- readRDS(tenx_data)
 tenx_labels <- read.csv(tenx_labels, header = T)
 
 #Filtering of single cell (C1 data)
+
 cat("Pre-processing single cell dataset (C1)")
 
 #Removing X in column names
+
 colnames(C1) <- sub("^X", "", colnames(C1))
 rownames(C1) <- C1[, 1]
 
 #Remove first column
+
 C1 <- C1[, -1]
 
-#Subsetting the mid-secretory cells
+#Subsetting cells, corresponding to the mid-secretory phase of the menstrual cycle
+
 C1_midsec_labels <- C1_labels[C1_labels$day == '21' | C1_labels$day == '22' | C1_labels$day == '24', ]
 C1_midsec <- C1[,colnames(C1) %in% C1_midsec_labels$cell_name]
 
 #Gene level filtering
 
 cat("Perfoming gene level filtering:")
+
 #Remove genes with 0 counts in all the cells
 C1_filt <- C1_midsec[rowSums(C1[]) > 0, ]
 sprintf("%i genes with 0 counts across all the cells were removed", nrow(C1_midsec) - nrow(C1_filt))
 sprintf("Now the expression matrix has %i genes", nrow(C1_filt))
 
 #Keep genes that are 'detectable' (have at least 10 counts) in at least 5% of cells
+
 keep_genes <- rowSums(C1_filt >= 0) >= round(0.05 * ncol(C1_filt))
 
 C1_filt2 <- C1_filt[keep_genes,]
@@ -56,6 +88,7 @@ sprintf("%i genes expressed in less than 10 cells were removed", nrow(C1_filt) -
 sprintf("Now the expression matrix has %i genes", nrow(C1_filt2))
 
 #Some editing of the labels
+
 C1_midsec_labels$cell_type <- sub(' ', '_', C1_midsec_labels$cell_type)
 
 #Filtering of single cell (10X data)
@@ -70,11 +103,13 @@ seurt[["percent.mito"]] <- PercentageFeatureSet(seurt, pattern = "^MT-")
 seurt_filt <- subset(seurt, subset = nFeature_RNA > 250 & nFeature_RNA < 4000 & percent.mito < 0.15)
 seurt_filt <- NormalizeData(object = seurt, normalization.method = "LogNormalize")
 
-#Subsetting the mid-secretory cells
+#Subsetting cells, corresponding to the mid-secretory phase of the menstrual cycle
+
 tenx_midsec_labels <- tenx_labels[tenx_labels$day == '20' | tenx_labels$day == '22' | tenx_labels$day == '23', ]
 seurt_midsec <- seurt_filt[,colnames(seurt_filt) %in% tenx_midsec_labels$cell_name]
 
 #Some editing of the labels
+
 tenx_midsec_labels$cell_type <- gsub(' ', '_', tenx_midsec_labels$cell_type)
 
 #Build signature from single-cell data (C1)
